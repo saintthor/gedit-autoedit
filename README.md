b47d1ccd64a0864f47b81755bef18ae29f2d265e
thor <zhuo.liu@newbeeair.com>
2016-09-01 14:11:28 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
+
+
+
 # gedit-autoedit
-autoedit the content in gedit with python console.
+gedit is a good text editor for ubuntu.
+
+this an example to edit content in gedit with it's python console.
+in python console, there is a 'window' parameter. with code:
+
+doc = window.get_active_document()
+start, end = doc.get_bounds()
+Text = doc.get_text( start, end, True )
+
+you can get the content of the active file. and with:
+
+doc.set_text( Text )
+
+you can update the content.
+
+
+## Start
+copy the gedit.py to one of sys.path then may import it from python console.
+
+## AutoReplace
+you may change the content with the AutoReplace function. type codes in python console:
+
+from gedit import AutoReplace as r
+def myReplace( oldText ):
+	return 'new text.'
+r( window, myReplace )
+
+the content will be changed.
+
+
+## AutoFormat
+the AutoFormat function is for chinese user.
+
+AutoFormat 用于将半角标点转为全角。如果 JoinLines = True，将删除所有单个的 \n。因为我处理的文档经常有多余的硬回车，删除单个的，保留连续多个的 \n，才能转为正常格式。用法：
+
+from gedit import AutoFormat as f
+f( window )
+
+## AutoNote
+至于 AutoNote 是处理文章中注释内容的，大多数人应该用不着。
+
+
---
+++ gedit.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 31 13:29:43 2016
+
+@author: thor
+"""
+import re
+
+def ReplaceSymbols( text ):
+    ""
+    for symbols in (( ',', '，' ), ( '.', '。' ), ( ';', '；' ), ( ':', '：' ), 
+                    ( '?', '？' ), ( '!', '！' ), ( '(', '（' ), ( ')', '）' )):
+        text = text.replace( *symbols )
+        
+    for OldSymb, ( LeftSymb, RightSymb ) in (( "'", ( "“", "”" )), ( '"', ( "‘", "’" ))):
+        if OldSymb in text:
+            text = text.replace( OldSymb, LeftSymb, 1 ).replace( OldSymb, RightSymb, 1 )
+    
+    return text
+
+def AutoFormat( window, JoinLines = True ):
+    ""
+    doc = window.get_active_document()
+    start, end = doc.get_bounds()
+    Text = doc.get_text( start, end, True )
+    
+    Text = ReplaceSymbols( Text )
+    if JoinLines:
+        Text = re.sub( r'(?<!\n)\n(?!\n)', '', Text )
+    
+    doc.set_text( Text )
+    
+
+def AutoNote( window, noteLines = (), labelPttn = r'\[(\d+)\]' ):
+    ""
+    if isinstance( noteLines, str ):
+        noteLines = list( filter( None, noteLines.split( '\n' )))
+        
+    doc = window.get_active_document()
+    start, end = doc.get_bounds()
+    Text = doc.get_text( start, end, True )
+    
+    Labels = map( int, re.findall( labelPttn, Text ))
+    
+    for i, lb in enumerate( Labels ):
+        if i + 1 != lb:
+            raise Exception( 'labels order error: %s' % lb )
+        note = u'[toggle=off][title][sup][注%d][/sup][/title]\n[content]%s[/content][/toggle]' % ( lb, noteLines[i] )
+        Text = re.sub( labelPttn, note, Text, 1 )
+        
+    Text = ReplaceSymbols( Text )
+    doc.set_text( Text )
+
+
+def AutoReplace( window, replaceFunc ):
+    ""
+    doc = window.get_active_document()
+    start, end = doc.get_bounds()
+    Text = doc.get_text( start, end, True )
+    doc.set_text( replaceFunc( Text ))
